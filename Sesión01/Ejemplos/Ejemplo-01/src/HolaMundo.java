131ea44beaf58b0d84e586228f4f5a3bdf2fa670
Gisellrom <romero.giselln@gmail.com>
2025-05-07 22:07:23 -0600

    primer ejemplo en java hola Java

---
+++ Sesión01/Ejemplos/Ejemplo-01/src/HolaMundo.java
@@ -0,0 +1,8 @@
+//Sesión 01: Imprimiendo un Hola Mundo
+//Nombre de la clase: Hola Mundo
+public class HolaMundo {
+    //Método principal
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Java!"); //mensaje en la consola
+    }
+}
