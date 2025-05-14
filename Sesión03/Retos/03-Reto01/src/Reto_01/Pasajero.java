48d5e719f80d205293b488a8e10adb17298facf7
Gisellrom <romero.giselln@gmail.com>
2025-05-13 23:45:01 -0600

    mueve ejemplos y reto a la carpeta de la sesión 03

---
+++ Sesión03/Retos/03-Reto01/src/Reto_01/Pasajero.java
@@ -0,0 +1,20 @@
+package Reto_01;
+
+public class Pasajero {
+    String nombre;
+    String pasaporte;
+
+    public Pasajero(String nombre, String pasaporte){
+        this.nombre = nombre;
+        this.pasaporte = pasaporte;
+    }
+
+    String getNombre(){
+        return this.nombre;
+    }
+
+    String getPasaporte(){
+        return this.pasaporte;
+    }
+
+}
